3639724b44d5f024a8668a5e9b685dca6cc42fae
rishabhjain2000 <rishabhjainnitbhopal@gmail.com>
2023-12-08 01:36:31 +0530

    starting

---
+++ comment.py
@@ -0,0 +1,33 @@
+#Single-Line Comments
+
+#This is a 'Single-Line Comment'
+
+
+#Multi-Line Comments:
+'''To write multi-line comments you can use ‘#’ at each line or 
+you can use the multiline string'''
+
+
+'''Escape Sequence Characters
+To insert characters that cannot be directly used in a string,
+ we use an escape sequence character.
+
+An escape sequence character is a backslash \ followed 
+by the character you want to insert.'''
+
+print("This will \" execute")
+
+
+'''
+Other Parameters of Print Statement
+object(s): Any object, and as many as you like. Will be
+ converted to string before printed
+
+sep='separator': Specify how to separate the objects, 
+if there is more than one. Default is ' '
+
+end='end': Specify what to print at the end. Default 
+is '\n' (line feed)
+
+file: An object with a write method. Default is sys.stdout
+'''
